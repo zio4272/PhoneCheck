3bb5922e0155a9a72eaf7155b661af8385dc0e53
zio4272 <zio4272@naver.com>
2017-10-12 13:00:12 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjeit/phonecheck/MainActivity.java
@@ -0,0 +1,25 @@
+package kr.co.tjeit.phonecheck;
+
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
+import android.telephony.TelephonyManager;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
